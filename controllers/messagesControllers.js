6e2b7b425bfac0835f634e08b3d805102e820780
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-26 11:47:21 +0100

    Work in progress

--- controllers/messagesControllers.js
+++ controllers/messagesControllers.js
@@ -1,19 +1,38 @@
-const Massage = require('../models/Message');
+const Message = require("../models/Message");
+const User = require("../models/User");
 
 module.exports = {
   getAll: async (req, res) => {
-    
+    const { date, user, text } = req.query;
+
+    let object;
+    let userForPost;
+
+    if (date) {
+      object = { date: date };
+    } else if (user) {
+      userForPost = await User.find({ name: { $regex: user } })
+        .populate("messages")
+        .exec((err, messages) => messages[0]?.messages);
+    } else if (text) {
+      object = { text: { $regex: text } };
+    } else {
+      object = {};
+    }
+
     try {
-      res.send("All Messages")
+      const messages = await Message.find();
+      res.json(userForPost);
     } catch (e) {
       console.log(e);
       res.sendStatus(404);
     }
   },
-  getById:  async (req, res) => {
-    
+  getById: async (req, res) => {
     try {
-      res.send("Massage By ID")
+      const { id } = req.params;
+      const message = await Message.findById(id);
+      res.json(message);
     } catch (e) {
       console.log(e);
       res.sendStatus(404);
