e61a2a6312349d08e0bf3cf85bbfbc060a37c5f4
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-26 10:37:02 +0100

    extand UserSchema with profile_img_url

--- models/User.js
+++ models/User.js
@@ -5,10 +5,11 @@ const UserSchema = new Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
+    profile_img_url: {type: String},
     messages: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message"
-    }]
+    }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
